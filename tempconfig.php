f6c4c167ec516e1d140171218aa0ba90e3bbb6d1
Kyle Eisenbarger <keisenb@ksu.edu>
2016-12-26 01:48:11 -0600

    Modified to work with new API

---
+++ tempconfig.php
@@ -0,0 +1,9 @@
+<?php
+    define('DBHOST', 'Database Host');
+    define('DBUSER', 'Database User');
+    define('DBPASS', 'Database password');
+    define('DBTABLE', 'Database Table');
+    define('TSID', 'Twilio SID');
+    define('TTOKEN', 'Twilio Token');
+    define('TNUMBER', '+Twilio Phone Number');
+?>
